ac8f9715abe0726d06245be3e002f25d2ab0f3ad
nishal <nishalk9399@gmail.com>
2024-09-04 14:52:48 +0530

    vpc creation as a module developer

---
+++ main.tf
@@ -0,0 +1,102 @@
+#here we are developing the script for all vpc modules and calling it from roboshop-infra simply module method 
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support = var.enable_dns_support
+
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags
+  )
+}
+
+#IG 
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags
+  )
+}
+
+#subnets 
+
+resource "aws_subnet" "public" {
+
+  count = length(var.public_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+
+
+  tags = merge(
+    var.common_tags, 
+    {
+        Name = var.public_subnet_names[count.index]
+    }
+  )
+}
+
+
+resource "aws_subnet" "private" {
+
+  count = length(var.private_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+
+
+  tags = merge(
+    var.common_tags, 
+    {
+        Name = var.private_subnet_names[count.index]
+    }
+  )
+}
+
+#route tables 
+
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+
+
+
+#   tags = {
+#     Name = "public_route_table-roboshop"
+#   }
+# }
+#  #private route table 
+
+
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+
+
+#  tags = {
+#     Name = "private_route_table-roboshop"
+#   }
+# }
+
+resource "aws_subnet" "database" {
+
+  count = length(var.database_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+
+
+  tags = merge(
+    var.common_tags, 
+    {
+        Name = var.database_subnet_names[count.index]
+    }
+  )
+}
